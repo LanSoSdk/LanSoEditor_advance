f3e0994ba45eee405a72092d2bcde7bd9488c3e0
sno_ON_Imac27 <support@lansongtech.com>
2019-09-02 15:06:17 +0800

    update limited time to 201910

---
+++ app/build/generated/source/buildConfig/debug/com/lansoeditor/advanceDemo/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.lansoeditor.advanceDemo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.lansoeditor.github";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 3600;
+  public static final String VERSION_NAME = "3.6.0";
+}
